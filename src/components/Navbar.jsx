d0d16485d79ce643b24c45df88edf061fa897ca4
anuj-1410 <anujagrawal1410@gmail.com>
2024-09-20 23:55:18 +0530

    navbar changed on 20/09

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,40 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom' ;
-import './Navbar.css'
-import {motion} from "framer-motion";
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+import { motion } from 'framer-motion';
 import logo from './images/logo.png';
+
 export default function Navbar() {
   return (
     <header>
-        <div className="left">
-          <img src={logo} alt="" height={60} width={60}/>
-          <li>AlgoKnot</li>
+      <div className="left">
+        <img src={logo} alt="Logo" height={60} width={60} />
+        <li>AlgoKnot</li>
+      </div>
+      <div className="middle">
+        <div className="allButtons">
+          <motion.div whileHover={{ backgroundColor: '#BEE9E8' }} whileTap={{ scale: 0.9 }}
+          className='link-home'>
+            <NavLink to="/" end
+            className={({ isActive }) => (isActive ? 'link link-home active' : 'link link-home')}
+            >Home</NavLink>
+          </motion.div>
+
+          <motion.div whileHover={{ backgroundColor: '#BEE9E8' }} whileTap={{ scale: 0.9 }}>
+            <NavLink to="/about"
+            className={({ isActive }) => (isActive ? 'link active' : 'link')}
+            >About</NavLink>
+          </motion.div>
+
+          <motion.div whileHover={{ backgroundColor: '#BEE9E8' }} whileTap={{ scale: 0.9 }}
+          className='link-courses'> 
+            <NavLink to="/courses"
+            className={({ isActive }) => (isActive ? 'link link-courses active' : 'link link-courses')}
+            >Courses</NavLink>
+          </motion.div>
         </div>
-        <div className="right">
-        <motion.li 
-          whileHover={{ scale: 1.1, backgroundColor : '#BEE9E8'}}
-          whileTap={{ scale: 0.9 }}
-        className='homeButton'><NavLink to={'/'} className={'link'}>Home</NavLink></motion.li>
-        <motion.li 
-          whileHover={{ scale: 1.1, backgroundColor : '#62B6CB'}}
-          whileTap={{ scale: 0.9 }}
-        className='aboutButton'><NavLink to={'/about'} className={'link'}>About</NavLink></motion.li>
-        {/* <motion.li 
-          whileHover={{ scale: 1.1, backgroundColor : '#5FA8D3'}}
-          whileTap={{ scale: 0.9 }}
-        className='contactButton'><NavLink to={'/Courses'} className={'link'}>Courses</NavLink></motion.li> */}
-        </div>
+      </div>
+      <div className="right"></div>
     </header>
-  )
+  );
 }
-
-
